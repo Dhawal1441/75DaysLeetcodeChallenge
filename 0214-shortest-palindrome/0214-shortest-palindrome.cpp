81396508d3044d95d4c001dc320b7060adc12714
Dhawal Gupta <guptavasu152@gmail.com>
2026-07-31 22:35:56 +0530

    Time: 0 ms (100%), Space: 13.2 MB (81.55%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        if (s.empty()) return s;
-
+        int n = s.size();
+        if (n <= 1) return s;
         string rev = s;
         reverse(rev.begin(), rev.end());
-
-        string temp = s + "#" + rev;
-        int n = temp.size();
-        vector<int> lps(n, 0);
-
-        for (int i = 1; i < n; i++) {
-            int j = lps[i - 1];
-            while (j > 0 && temp[i] != temp[j]) {
-                j = lps[j - 1];
-            }
-            if (temp[i] == temp[j]) {
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (rev[i] == s[j]) {
                 j++;
             }
-            lps[i] = j;
         }
-
-        int longestPalindromePrefixLen = lps.back();
-        string remainingSuffix = s.substr(longestPalindromePrefixLen);
-        reverse(remainingSuffix.begin(), remainingSuffix.end());
-
-        return remainingSuffix + s;
+        if(j == n ) return s;
+        string suffix = s.substr(j);
+        string prefix = suffix;
+        reverse(prefix.begin(), prefix.end());
+        return prefix + shortestPalindrome(s.substr(0, j)) + suffix;
     }
 };
